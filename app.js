c6e3976a90a5737f496e9b97e81c9f8b6d75da03
Eduardo Garcia <84125798+edugn2298@users.noreply.github.com>
2024-07-03 07:04:55 -0400

    act

--- app.js
+++ app.js
@@ -19,3 +19,64 @@
   En caso de que los datos no sean válidos, se debe de mostrar un mensaje de error
   debajo del campo que no cumpla con la validación.
   */
+
+const expresiones = {
+  nombre: /^[a-zA-ZÀ-Öà-ö-_ ]+$/, // Permite letras, guiones bajos y espacios
+  correo: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, // Correo electrónico estándar
+  telefono: /^\+?[0-9]{6,15}$/, // Número de teléfono internacional
+  contrasena: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[¡¿?@$%'#&*])\S{8,}$/, // Contraseña segura
+  pasaporte: /^[A-Za-z0-9]{6,15}$/, // Pasaporte alfanumérico (formato puede variar)
+};
+
+const formulario = document.querySelector('#formulario');
+const inputs = document.querySelectorAll('input');
+const contrasena = document.querySelector('#contrasena');
+const contrasena2 = document.querySelector('#confirmarContrasena');
+
+function validarFormulario(e){
+  console.log(e.target.name);
+  switch(e.target.name){
+    case "nombre":
+      validarCampo( expresiones.nombre, e.target, 'nombre');
+    break;
+    case "correo":
+      validarCampo( expresiones.correo, e.target, 'correo');
+    break;
+    case "telefono":
+      validarCampo( expresiones.telefono, e.target, 'telefono');
+    break;
+    case "contrasena":
+      validarCampo( expresiones.contrasena, e.target, 'contrasena');
+    break;
+    case "confirmarContrasena":
+      validarContrasena();
+    break;
+    case "pasaporte":
+      validarCampo( expresiones.pasaporte, e.target, 'pasaporte');
+    break;
+  }
+
+}
+
+function validarCampo(expresiones, input, campo){
+  if (expresiones.test(input.value)){
+    console.log('Valido');
+  } else {
+    console.log('Invalido');
+  }
+}
+
+function validarContrasena(){
+  if (contrasena.value == contrasena2.value){
+
+  } else if (contrasena.value != contrasena2.value){
+    
+  }
+}
+
+
+inputs.forEach( input => input.addEventListener('keyup', validarFormulario));
+
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
+})
